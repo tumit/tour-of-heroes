a125baac1f690d7cd3a2bda6c2de6f2e99a31259
tumit <tumit@odd.works>
2017-08-26 00:46:08 +0700

    show the hero

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,16 +21,33 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Tour of Heroes'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Tour of Heroes');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Tour of Heroes');
   }));
+
+  it(`should have as hero`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.hero).toEqual({
+      id: 1,
+      name: 'Windstorm'
+    });
+  }));
+
+  it('should render hero details in a h2 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Windstorm details!');
+  }));
+
 });
