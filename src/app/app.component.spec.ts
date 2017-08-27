0a5a8e63c9645d210e7ada6dd22383249ea1e0e5
tumit <tumit@odd.works>
2017-08-28 00:27:33 +0700

    List heroes with ngFor

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,10 +3,14 @@ import { RouterTestingModule } from '@angular/router/testing'
 
 import { AppComponent } from './app.component'
 import { FormsModule } from '@angular/forms'
-import { tick } from "@angular/core/testing"
-import { fakeAsync } from "@angular/core/testing"
+import { tick } from '@angular/core/testing'
+import { fakeAsync } from '@angular/core/testing'
 
 describe('AppComponent', () => {
+
+  let fixture: any
+  let compiled: any
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -17,29 +21,27 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents()
+    fixture = TestBed.createComponent(AppComponent)
+    fixture.detectChanges()
+    compiled = fixture.debugElement.nativeElement
   }))
 
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.debugElement.componentInstance
     expect(app).toBeTruthy()
   }))
 
   it(`should have as title 'Tour of Heroes'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.debugElement.componentInstance
     expect(app.title).toEqual('Tour of Heroes')
   }))
 
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent)
     fixture.detectChanges()
-    const compiled = fixture.debugElement.nativeElement
     expect(compiled.querySelector('h1').textContent).toContain('Tour of Heroes')
   }))
 
   it(`should have as hero`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.debugElement.componentInstance
     expect(app.hero).toEqual({
       id: 1,
@@ -48,31 +50,39 @@ describe('AppComponent', () => {
   }))
 
   it('should render hero details in a h2 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent)
-    fixture.detectChanges()
-    const compiled = fixture.debugElement.nativeElement
     expect(compiled.querySelector('h2').textContent)
       .toContain('Windstorm details!')
   }))
 
-  it('should render hero id in a span.hero-id tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent)
-    fixture.detectChanges()
-    const compiled = fixture.debugElement.nativeElement
-    expect(compiled.querySelector('span.hero-id').textContent)
-      .toContain('1')
+  it('should render hero id in a span.hero-id', async(() => {
+    expect(
+      compiled.querySelector('span.hero-id').textContent
+    ).toContain('1')
   }))
 
-  it('should render hero name in a input.hero-name tag', fakeAsync(() => {
-    const fixture = TestBed.createComponent(AppComponent)
-    fixture.detectChanges()
-    const compiled = fixture.debugElement.nativeElement
+  it('should render hero name in a input.hero-name', fakeAsync(() => {
     tick()
     expect(
-      compiled
-        .querySelector('input.hero-name')
-        .value
+      compiled.querySelector('input.hero-name').value
     ).toBe('Windstorm')
   }))
 
+  it(`should render 'My heroes' in a h2 tag`, async(() => {
+    expect(compiled.querySelector('h2.my-heroes').textContent)
+      .toContain('My Heroes')
+  }))
+
+  it('should render heroes in span.badge', fakeAsync(() => {
+    tick()
+    expect(
+      compiled.querySelectorAll('span.badge').length
+    ).toBe(10)
+  }))
+
+  it('should render heroes in ul>li:first-child', fakeAsync(() => {
+    tick()
+    expect(
+      compiled.querySelector('ul>li:first-child').textContent.trim()
+    ).toBe('11 Mr. Nice')
+  }))
 })
